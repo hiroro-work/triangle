a6c9fbe06bc9d8eb64616968b8e8b28d8a4ae9c6
hiroro-work <hiroro.work@gmail.com>
2018-07-08 12:06:32 +0000

    update spec/triangle_spec.rb

--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -1,22 +1,22 @@
 require File.expand_path(File.dirname(__FILE__) + '/../triangle')
 
 describe Triangle do
-  specify { expect{Triangle.new("2, 3".split).run}.to output("三角形じゃないです＞＜\n").to_stdout }
-  specify { expect{Triangle.new("2, 3, 4, 5".split).run}.to output("三角形じゃないです＞＜\n").to_stdout }
-  specify { expect{Triangle.new("a, 3, 4".split).run}.to output("三角形じゃないです＞＜\n").to_stdout }
-  specify { expect{Triangle.new("2, b, 4".split).run}.to output("三角形じゃないです＞＜\n").to_stdout }
-  specify { expect{Triangle.new("2, 3, c".split).run}.to output("三角形じゃないです＞＜\n").to_stdout }
-  specify { expect{Triangle.new("0, 3, 4".split).run}.to output("三角形じゃないです＞＜\n").to_stdout }
-  specify { expect{Triangle.new("2, 0, 4".split).run}.to output("三角形じゃないです＞＜\n").to_stdout }
-  specify { expect{Triangle.new("2, 3, 0".split).run}.to output("三角形じゃないです＞＜\n").to_stdout }
-  specify { expect{Triangle.new("1, 2, 3".split).run}.to output("三角形じゃないです＞＜\n").to_stdout }
-  specify { expect{Triangle.new("2, 3, 4".split).run}.to output("不等辺三角形ですね！\n").to_stdout }
-  specify { expect{Triangle.new("4, 2, 3".split).run}.to output("不等辺三角形ですね！\n").to_stdout }
-  specify { expect{Triangle.new("12, 13, 14".split).run}.to output("不等辺三角形ですね！\n").to_stdout }
-  specify { expect{Triangle.new("1, 2, 2".split).run}.to output("二等辺三角形ですね！\n").to_stdout }
-  specify { expect{Triangle.new("2, 1, 2".split).run}.to output("二等辺三角形ですね！\n").to_stdout }
-  specify { expect{Triangle.new("2, 2, 1".split).run}.to output("二等辺三角形ですね！\n").to_stdout }
-  specify { expect{Triangle.new("10, 30, 30".split).run}.to output("二等辺三角形ですね！\n").to_stdout }
-  specify { expect{Triangle.new("1, 1, 1".split).run}.to output("正三角形ですね！\n").to_stdout }
-  specify { expect{Triangle.new("100, 100, 100".split).run}.to output("正三角形ですね！\n").to_stdout }
+  specify { expect{Triangle.new(%w(2, 3)).run}.to output("三角形じゃないです＞＜\n").to_stdout }
+  specify { expect{Triangle.new(%w(2, 3, 4, 5)).run}.to output("三角形じゃないです＞＜\n").to_stdout }
+  specify { expect{Triangle.new(%w(a, 3, 4)).run}.to output("三角形じゃないです＞＜\n").to_stdout }
+  specify { expect{Triangle.new(%w(2, b, 4)).run}.to output("三角形じゃないです＞＜\n").to_stdout }
+  specify { expect{Triangle.new(%w(2, 3, c)).run}.to output("三角形じゃないです＞＜\n").to_stdout }
+  specify { expect{Triangle.new(%w(0, 3, 4)).run}.to output("三角形じゃないです＞＜\n").to_stdout }
+  specify { expect{Triangle.new(%w(2, 0, 4)).run}.to output("三角形じゃないです＞＜\n").to_stdout }
+  specify { expect{Triangle.new(%w(2, 3, 0)).run}.to output("三角形じゃないです＞＜\n").to_stdout }
+  specify { expect{Triangle.new(%w(1, 2, 3)).run}.to output("三角形じゃないです＞＜\n").to_stdout }
+  specify { expect{Triangle.new(%w(2, 3, 4)).run}.to output("不等辺三角形ですね！\n").to_stdout }
+  specify { expect{Triangle.new(%w(4, 2, 3)).run}.to output("不等辺三角形ですね！\n").to_stdout }
+  specify { expect{Triangle.new(%w(12, 13, 14)).run}.to output("不等辺三角形ですね！\n").to_stdout }
+  specify { expect{Triangle.new(%w(1, 2, 2)).run}.to output("二等辺三角形ですね！\n").to_stdout }
+  specify { expect{Triangle.new(%w(2, 1, 2)).run}.to output("二等辺三角形ですね！\n").to_stdout }
+  specify { expect{Triangle.new(%w(2, 2, 1)).run}.to output("二等辺三角形ですね！\n").to_stdout }
+  specify { expect{Triangle.new(%w(10, 30, 30)).run}.to output("二等辺三角形ですね！\n").to_stdout }
+  specify { expect{Triangle.new(%w(1, 1, 1)).run}.to output("正三角形ですね！\n").to_stdout }
+  specify { expect{Triangle.new(%w(100, 100, 100)).run}.to output("正三角形ですね！\n").to_stdout }
 end
